c412b92a1f3a26e59241bae7956faafbe4fd297d
nguyen thanh hau <thanhhau.nguyen@cqumail.com>
2021-06-23 13:44:38 +1000

    innitialize code

--- server.js
+++ server.js
@@ -3,3 +3,12 @@ const express = require('express');
 
 const app = express();
 
+app.get("/",(req,res) => {
+    res.send("API is running");
+
+} 
+)
+
+const PORT = process.env.PORT || 5000;   // default port
+
+app.listen(PORT, () => console.log(`Server start on port ${PORT}`));
